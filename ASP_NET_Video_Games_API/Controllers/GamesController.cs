c1f9d4e8025e0f20957d6a829c7955eed32c17c6
Garrett <ga.landin89@gmail.com>
2022-04-26 12:12:42 -0500

    Deleted ExampleController file and created GamesController file + Created get all publishers end point + created get games by publisher end point

--- ASP_NET_Video_Games_API/Controllers/GamesController.cs
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -4,14 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Video_Games_API.Controllers
 {
-    // api/examples
     [Route("api/[controller]")]
     [ApiController]
-    public class ExamplesController : ControllerBase
+    public class GamesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public ExamplesController(ApplicationDbContext context)
+        public GamesController(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -25,11 +24,14 @@ namespace ASP_NET_Video_Games_API.Controllers
         }
 
         [HttpGet("{pubName}")]
+
         public IActionResult GetGamesByPublisher(string pubName)
         {
+            int? maxYear = _context.VideoGames.Select(vg => vg.Year).Max();
+            int? minYear = _context.VideoGames.Select(vg => vg.Year).Min();
+
             var videoGames = _context.VideoGames.Where(vg => vg.Publisher == pubName);
             return Ok(videoGames);
         }
-
     }
 }
